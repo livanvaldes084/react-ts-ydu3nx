3e929749cd6b0eeaad3a1b3dc9d1b1f04a0b1420
livanvaldes084 <livanvaldes084@gmail.com>
2022-06-29 22:54:43 -0300

    1 file modified, 3 added

---
+++ src/settings/Voices/components/Accordion.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import{ useState, useRef } from "react"
+import ChevronClosed from "../icons/ChevronClosed.svg"
+import ChevronOpen from "../icons/ChevronOpen.svg"
+type AccordionProps = {
+  title: string
+  content: string
+}
+const Accordion = ({ title, content }: AccordionProps) => {
+  const [isOpened, setOpened] = useState<boolean>(false)
+  const [height, setHeight] = useState<string>("0px")
+  const contentElement = useRef(null)
+
+  const HandleOpening = () => {
+    setOpened(!isOpened)
+    setHeight(!isOpened ? `${contentElement.current.scrollHeight}px` : "0px")
+  }
+  return (
+    <div onClick={HandleOpening} className="border border-indigo-400">
+      <div className={"bg-indigo-300 p-4 flex justify-between text-white"}>
+        <h4 className="font-semibold">{title}</h4>
+        {isOpened ? <ChevronOpen /> : <ChevronClosed />}
+      </div>
+      <div
+        ref={contentElement}
+        style={{ height: height }}
+        className="bg-gray-200 overflow-hidden transition-all duration-200"
+      >
+        <p className="p-4">{content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Accordion
--- src/settings/Voices/index.tsx
+++ src/settings/Voices/index.tsx
@@ -1,5 +1,22 @@
 import * as React from 'react';
+import Accordion from "./components/Accordion"
 
 export default function Voice() {
+  const data = [
+    {
+      title: "How much do i need to pay for this?",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo."
+    },
+    {
+      title: "Can i delete my account?",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo."
+    },
+    {
+      title: "Is this accordion styled in Tailwind css?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    }
+  ]
   return <h2 className="text-4xl">BrainHi Voice IVR</h2>;
 }
