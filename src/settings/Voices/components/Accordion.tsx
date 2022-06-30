c549322df3e992dca7b0aaab60839a085730e61c
livanvaldes084 <livanvaldes084@gmail.com>
2022-06-29 23:28:40 -0300

    4 files modified

--- src/settings/Voices/components/Accordion.tsx
+++ src/settings/Voices/components/Accordion.tsx
@@ -1,35 +1,70 @@
-import * as React from "react"
-import{ useState, useRef } from "react"
-import ChevronClosed from "../icons/ChevronClosed.svg"
-import ChevronOpen from "../icons/ChevronOpen.svg"
+import * as React from 'react';
+import { useState, useRef } from 'react';
+
 type AccordionProps = {
-  title: string
-  content: string
-}
+  title: string;
+  content: string;
+};
 const Accordion = ({ title, content }: AccordionProps) => {
-  const [isOpened, setOpened] = useState<boolean>(false)
-  const [height, setHeight] = useState<string>("0px")
-  const contentElement = useRef(null)
+  const [isOpened, setOpened] = useState<boolean>(false);
+  const [height, setHeight] = useState<string>('0px');
+  const contentElement = useRef(null);
 
   const HandleOpening = () => {
-    setOpened(!isOpened)
-    setHeight(!isOpened ? `${contentElement.current.scrollHeight}px` : "0px")
-  }
+    setOpened(!isOpened);
+    setHeight(!isOpened ? `${contentElement.current.scrollHeight}px` : '0px');
+  };
   return (
-    <div onClick={HandleOpening} className="border border-indigo-400">
-      <div className={"bg-indigo-300 p-4 flex justify-between text-white"}>
-        <h4 className="font-semibold">{title}</h4>
-        {isOpened ? <ChevronOpen /> : <ChevronClosed />}
+    <div className="flex flex-row items-center">
+      <div onClick={HandleOpening} className="items-center">
+        {isOpened ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        )}
       </div>
-      <div
-        ref={contentElement}
-        style={{ height: height }}
-        className="bg-gray-200 overflow-hidden transition-all duration-200"
-      >
-        <p className="p-4">{content}</p>
+      <div>
+        <div className="border border-indigo-400 rounded">
+          <div className={'bg-indigo-300 p-4 flex justify-between text-white'}>
+            <h4 className="font-semibold">{title}</h4>
+          </div>
+          <div
+            ref={contentElement}
+            style={{ height: height }}
+            className="bg-gray-200 overflow-hidden transition-all duration-200"
+          >
+            <p className="p-4">{content}</p>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;
--- src/settings/Voices/index.tsx
+++ src/settings/Voices/index.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import Accordion from "./components/Accordion"
+import Accordion from './components/Accordion';
 
 export default function Voice() {
   const data = [
     {
-      title: "How much do i need to pay for this?",
+      title: 'How much do i need to pay for this?',
+      order: 1,
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo."
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo.',
     },
     {
-      title: "Can i delete my account?",
+      title: 'Can i delete my account?',
+      order: 2,
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo."
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo.',
     },
-    {
-      title: "Is this accordion styled in Tailwind css?",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-    }
-  ]
-  return <h2 className="text-4xl">BrainHi Voice IVR</h2>;
+  ];
+  return (
+    <div>
+      <h2 className="text-4xl">BrainHi Voice IVR</h2>
+      <div className="flex flex-col container gap-4">
+        {data.map((item) => (
+          <Accordion title={item.title} content={item.content} />
+        ))}
+      </div>
+    </div>
+  );
 }
