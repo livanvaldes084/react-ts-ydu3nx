27b520d0e5b4b9826cc84e637eb67b420eb31978
livanvaldes084 <livanvaldes084@gmail.com>
2022-06-30 10:30:24 -0300

    1 file modified, 2 deleted

--- src/settings/Voices/components/Accordion.tsx
+++ src/settings/Voices/components/Accordion.tsx
@@ -51,28 +51,30 @@ const Accordion = ({ title, order, content }: AccordionProps) => {
         )}
       </div>
       <div className="border border-indigo-400 rounded">
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row items-center">
           <div className="text-white bg-indigo-400 p-2">{order}</div>
-          <h4 className="text-blue-500">{title}</h4>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
+          <div className="flex flex-row justify-between">
+            <h4 className="text-blue-500">{title}</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
         </div>
         <div
           ref={contentElement}
